02504f1b4f5ce539fbfd5db369ff0f231bbfe983
= <daporto@uninorte.edu.co>
2024-10-18 12:11:12 -0400

    refactor: :zap: showPLaceInformation function was created

--- script.js
+++ script.js
@@ -4,33 +4,27 @@ const getPostalCodeInfo = async () => {
     try {
         const response = await fetch(url);
         if (!response.ok) {
-            throw new Error(`Something went wrong making the request`);
+            throw new Error(`The postal code ${postalCode} was not found`);
         }
         const data = await response.json();
         const placeInfo = data.places[0];
-        const stateAbbreviation = placeInfo['state abbreviation'];
-        document.getElementById('latitude').textContent = `Latitude: ${placeInfo.latitude}`;
-        document.getElementById('longitude').textContent = `Longitude: ${placeInfo.longitude}`;    
-        document.getElementById('placeName').textContent = `Place name: ${placeInfo['place name']}`;
-        document.getElementById('state').textContent = `State: ${placeInfo.state}`;    
-        document.getElementById('stateAbbreviation').textContent = `State abbreviation: ${stateAbbreviation}`;
-        const stateImage = `<img id='stateImage' src="/states/${stateAbbreviation}.svg" alt="State Image" width="200" height="200">`;
-        const currentImage = document.getElementById('stateImage');
-        if(currentImage) currentImage.remove();
-        const container = document.getElementById('imageContainer');
-        container.style.display = 'flex';
-        container.insertAdjacentHTML('beforeend', stateImage);
+        showPLaceInformation(placeInfo);
     } catch (error) {
         alert(error.message)
     }
 }
 
-const cleanFields = () => {
-    document.getElementById('latitude').textContent = 'latitude: ';
-    document.getElementById('longitude').textContent = 'longitude:';
-    document.getElementById('placeName').textContent = 'Place name: ';
-    document.getElementById('state').textContent = 'State: ';
-    document.getElementById('stateAbbreviation').textContent = 'State abbreviation: ';
-    const stateImage = document.getElementById('stateImage');
-    if(stateImage) stateImage.remove();
+const showPLaceInformation = (placeInfo) => {
+    const stateAbbreviation = placeInfo['state abbreviation'];
+    document.getElementById('latitude').textContent = `Latitude: ${placeInfo.latitude}`;
+    document.getElementById('longitude').textContent = `Longitude: ${placeInfo.longitude}`;    
+    document.getElementById('placeName').textContent = `Place name: ${placeInfo['place name']}`;
+    document.getElementById('state').textContent = `State: ${placeInfo.state}`;    
+    document.getElementById('stateAbbreviation').textContent = `State abbreviation: ${stateAbbreviation}`;
+    const stateImage = `<img id='stateImage' src="/states/${stateAbbreviation}.svg" alt="State Image" width="200" height="200">`;
+    const currentImage = document.getElementById('stateImage');
+    if(currentImage) currentImage.remove();
+    const container = document.getElementById('imageContainer');
+    container.style.display = 'flex';
+    container.insertAdjacentHTML('beforeend', stateImage);
 }
